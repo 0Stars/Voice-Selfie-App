e8701b03064ec0482b18c01fd48070e64c7c090d
Code-studio456 <79489448+Code-studio456@users.noreply.github.com>
2021-06-04 18:51:04 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+var Textbox = document.getElementById("textbox"); 
+
+function start()
+{
+    Textbox.innerHTML = ""; 
+    recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+}
